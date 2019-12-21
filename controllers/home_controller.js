0b4a0b9d7b65e8e88710a8cd81cd93d93469dde5
Avinash <avinash999yadav@gmail.com>
2019-12-21 11:44:58 +0530

    add multiple routers and controller user and profile

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,5 @@
 module.exports.home = function(req,res){
- console.log("hi home comtroller");
+ 
 return res.end('<h1>Express is up for codeial </h1>');
 
 
---
+++ controllers/users_controller.js
@@ -0,0 +1,6 @@
+module.exports.profile = function(req,res){
+ 
+    return res.end('<h1>User profile </h1>');
+    
+    
+    }
